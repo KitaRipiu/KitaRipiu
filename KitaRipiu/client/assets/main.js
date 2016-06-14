86f08a11527e6b2fed7987c370f9315cbfbca907
riskteria <riskteria@gmail.com>
2016-06-14 12:44:56 +0700

    Insert the frontend to the backend

---
+++ KitaRipiu/client/assets/main.js
@@ -0,0 +1,41 @@
+$('.menu .item')
+  .tab()
+;
+
+$(document)
+    .ready(function() {
+
+      // fix menu when passed
+      $('.masthead')
+        .visibility({
+          once: false,
+          onBottomPassed: function() {
+            $('.fixed.menu').transition('fade in');
+          },
+          onBottomPassedReverse: function() {
+            $('.fixed.menu').transition('fade out');
+          }
+        })
+      ;
+
+      // create sidebar and attach to menu open
+      $('.ui.sidebar')
+        .sidebar('attach events', '.toc.item')
+      ;
+
+    })
+  ;
+
+$("#showsearch1").click(function () {
+            $('.ui.modal').modal('show');
+        });
+
+$("#showsearch2").click(function () {
+            $('.ui.modal').modal('show');
+        });
+
+$("#showsearch3").click(function () {
+            $('.ui.modal').modal('show');
+        });
+
+$('.ui.embed').embed();
---
+++ KitaRipiu/client/router.js
@@ -0,0 +1,28 @@
+// Set the route group for public users
+const publicRoute = FlowRouter.group({ name: 'public' });
+
+// Set the route group for admininstrator
+const adminRoute  = FlowRouter.group({ name: 'admin', prefix: '/admin' });
+
+// Home route
+publicRoute.route('/',
+  { name: 'Home',       action: renderView });
+// Smartphone route
+publicRoute.route('/smartphone',
+  { name: 'Smartphone', action: renderView });
+// Read article route
+publicRoute.route('/read/:article',
+  { name: 'Read',       action: renderView });
+// Login route
+publicRoute.route('/login',
+  { name: 'Login',      action: renderView });
+
+function renderView() {
+  renderMainLayoutWith(this.name);
+  console.log("[FlowRouter] params", this.name, FlowRouter._current.params);
+}
+
+// Render the router layouts
+function renderMainLayoutWith(view) {
+  BlazeLayout.render("PublicLayout", {main : view});
+}
